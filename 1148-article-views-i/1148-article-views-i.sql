057d791bf13f50df4d9feb3c40b67a9574b34c54
Venkatesh Kastala <60286831+VenkateshKasthala@users.noreply.github.com>
2026-06-09 23:05:09 -0400

    Time: 402 ms (89.02%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select distinct author_id as id 
+from views
+where author_id = viewer_id 
+order by 1 ;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
