5ca64aa9c6ebc441b5bdf1b3d1e4f22e07d6a4ea
Venkatesh Kastala <60286831+VenkateshKasthala@users.noreply.github.com>
2026-06-09 22:28:29 -0400

    Time: 543 ms (55.19%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# The query filters rows where both low_fats and recyclable are 'Y' and returns the product_id.
+Select product_id from products where low_fats ='Y' and recyclable = 'Y';
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
