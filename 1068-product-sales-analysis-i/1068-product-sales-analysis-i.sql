ec81c4950954180bc0206fc3c70c9673d51d5c7a
Venkatesh Kastala <60286831+VenkateshKasthala@users.noreply.github.com>
2026-06-11 11:54:33 -0400

    Time: 1719 ms (5.46%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -0,0 +1,8 @@
+SELECT P.PRODUCT_NAME, S.YEAR, S.PRICE
+FROM SALES S JOIN PRODUCT P 
+ON P.PRODUCT_ID = S.PRODUCT_ID;
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
