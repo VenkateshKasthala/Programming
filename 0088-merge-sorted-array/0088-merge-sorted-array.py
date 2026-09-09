9f29344b4ba964067b61635e950459404762e3e3
Venkatesh Kastala <60286831+VenkateshKasthala@users.noreply.github.com>
2026-09-08 21:27:49 -0400

    Time: 0 ms (100%), Space: 19.3 MB (39.22%) - LeetHub - Time: O(m + n), Space: O(1)

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,56 +1,24 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
-        # # First Attempt- Well this is not the Optimal
-        # j=0
-        # for i in range(m,len(nums1)):
-        #     # Start looping from m - if there are elements in nums1, usually zeroes (replace them). If nothing is present append
-        #     if nums1[i]==0:
-        #         nums1[i] = nums2[j]
-        #         j=j+1
-        # # we have to sort it now
-        # nums1.sort()
-
-
-# Optimal Solution
         
-#Instead of starting from the beginning (which would require shifting elements), we compare the largest values of both arrays and place them at the very end of nums1.
+        p1 = m - 1          # Last valid element in nums1
+        p2 = n - 1          # Last element in nums2
+        p_merge = m + n - 1 # Last overall slot in nums1
 
-        i = m-1
-        j = n-1
-        k = m+n-1
-
-        while i>=0 and j>=0:
-            if nums1[i]>nums2[j]:
-                nums1[k]=nums1[i]
-                i-=1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p_merge] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[k]=nums2[j]
-                j=j-1
+                nums1[p_merge] = nums2[p2]
+                p2 -= 1
+            p_merge -= 1
             
-            k=k-1
+        while p2 >= 0:
+            nums1[p_merge] = nums2[p2]
+            p2 -= 1
+            p_merge -= 1
 
-        # Edge case
-        # if elements in nums1 are leftover(i>0) - They are sorted anyway!
-
-        while j>=0:
-            nums1[k]=nums2[j]
-            j=j-1
-            k=k-1 
-        
-
-
-
-
-
-
-
-            
-                
-            
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
