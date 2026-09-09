cfbf4b3af778769e7d62b381734a79512dc18785
Venkatesh Kastala <60286831+VenkateshKasthala@users.noreply.github.com>
2026-09-08 21:55:46 -0400

    Time: 0 ms (100%), Space: 19.3 MB (39.22%) - LeetHub - Time: O(m + n), Space: O(1)

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,23 +1,24 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        
-        p1 = m - 1          # Last valid element in nums1
-        p2 = n - 1          # Last element in nums2
-        p_merge = m + n - 1 # Last overall slot in nums1
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m-1
+        j = n-1
+        k = m+n-1
 
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[p_merge] = nums1[p1]
-                p1 -= 1
+        while i>=0 and j>=0:
+            if nums1[i]>nums2[j]:
+                nums1[k] = nums1[i]
+                i=i-1
             else:
-                nums1[p_merge] = nums2[p2]
-                p2 -= 1
-            p_merge -= 1
-            
-        while p2 >= 0:
-            nums1[p_merge] = nums2[p2]
-            p2 -= 1
-            p_merge -= 1
+                nums1[k]=nums2[j]
+                j=j-1
+            k=k-1
+        while j>=0:
+            nums1[k]=nums2[j]
+            j=j-1
+            k=k-1
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
