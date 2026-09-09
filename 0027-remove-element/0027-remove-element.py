252bebb36520c0e7363f006a5146f8372e15faed
Venkatesh Kastala <60286831+VenkateshKasthala@users.noreply.github.com>
2026-09-09 18:28:33 -0400

    Time: 0 ms (100%), Space: 19.3 MB (18.9%) - LeetHub - Time: O(n), Space: O(1)

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,24 +1,12 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-
-        # Input: nums = [0,1,2,2,3,0,4,2], val = 2
-        # Output: 5, nums = [0,1,4,0,3,_,_,_] 
-        k = 0 
-        
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-            else:
-                nums.append('_')
-                
-        
-        # for i in range(k, len(nums)):
-        #     nums[i] = '_'
-            
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k=0
+        for i in nums:
+            if i!= val:
+                nums[k] = i
+                k+=1
         return k
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
