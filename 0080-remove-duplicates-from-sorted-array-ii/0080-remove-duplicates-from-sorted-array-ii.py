3f8d380c6fc23d201ff4360b650c87adaec5bc6b
Venkatesh Kastala <60286831+VenkateshKasthala@users.noreply.github.com>
2026-09-09 21:12:23 -0400

    Time: 76 ms (94.75%), Space: 22.1 MB (64.76%) - LeetHub - Time: O(n), Space: O(1)

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,17 +1,21 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if len(nums) <= 2:
-            return len(nums)
-        
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # k = 1
+        # for i in range(1, len(nums)):
+        #     if nums[i] != nums[i - 1]:
+        #         nums[k] = nums[i]
+        #         k += 1
+                
+        # return k
+
         k=2
-        for i in range(2,len(nums)):
+        for i in range(2, len(nums)):
             if nums[i]!=nums[k-2]:
-            # Initially I hit the wall by using i-2 
-            ## Look closely at Step 4 (where i=4) for case 1.The array now looks like this-  [1, 1, 2, 2, 2, 3]
-                nums[k]=nums[i]
-                k=k+1
+               nums[k]=nums[i]
+               k+=1
+
         return k
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
