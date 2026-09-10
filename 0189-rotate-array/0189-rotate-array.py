3f5503d389309b16fdbbd5ab8c96baa480166e09
Venkatesh Kastala <60286831+VenkateshKasthala@users.noreply.github.com>
2026-09-10 19:15:42 -0400

    Time: 142 ms (17.6%), Space: 34.9 MB (43.17%) - LeetHub - Time: O(n), Space: O(1)

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,8 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k %= len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
+        # k %= len(nums)
+        # nums[:] = nums[-k:] + nums[:-k]
+        n=len(nums)
+        k%=n
+
+        def reverse(start: int, end: int) -> None:
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
